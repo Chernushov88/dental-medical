2e1370cb1c8dc6a0f2dc9e54db59ce3bb530892c
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-07-28 10:59:44 +0300

    feat: add user dropdown component and put it to the header

--- src/components/general/HeaderComponent.vue
+++ src/components/general/HeaderComponent.vue
@@ -3,11 +3,12 @@
 </script>
 <template>
     <header class="max-w-header w-full mx-auto px-10 py-4.5 bg-blue-light rounded-lg my-10 flex atems-center justify-between">
-        <nuxt-link to="/">
+        <nuxt-link to="/" class="flex items-center" >
             <img src="~/assets/images/logo.svg" alt="">
         </nuxt-link>
 
         <NavBarComponent />
+        <UserDropdownComponent />
         
         <div>
             Auth Bar
---
+++ src/components/general/UserDropdownComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div class="relative">
+            <div class="roundee-full overflow-hidden h-14 w-14">
+                <img 
+                    src="/user/user_icon.png" 
+                    :alt="`User Icon ${user.firstName} ${user.lastName}`" />
+            </div>
+            <div 
+                v-if="user.active"
+                class="absolute top-1 right-1 bg-green-500 rounded-full h-2.5 w-2.5 border border-white"></div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const user = ref({
+    active: true,
+    firstName: 'Serhii',
+    lastName: 'Chernyshov',
+    icon: '/user/user_icon.png',
+})
+</script>
+
+<style scoped>
+
+</style>
