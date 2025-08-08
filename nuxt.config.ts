1c7f416ab766a0a8f8b650e88788422ae087e589
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-08-08 18:36:06 +0300

    chore: add slider to services section on the home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,7 @@ export default defineNuxtConfig({
   ],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss',    
-  ],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-swiper'],
   srcDir: 'src/.',
   components: {
     dirs: [
--- src/components/pages/home/services/ServicesSliderSectionComponent.vue
+++ src/components/pages/home/services/ServicesSliderSectionComponent.vue
@@ -1,37 +1,92 @@
 <template>
-    <SectionComponent class="bg-blue-light py-12.5 px-5 mb-40 lg:rounded-2lg lg:px-8 flex flex-col lg:flex-row gap-6">
-        <ServiceCard 
-            v-for="item in items"
-            :key="item.title"
-            :item="item"
-        />
-    </SectionComponent>
+  <SectionComponent
+    class="bg-blue-light py-12.5 px-5 mb-40 lg:rounded-2lg lg:px-8 flex flex-col lg:flex-row gap-6"
+  >
+    <div class="lg:hidden">
+      <ClientOnly>
+        <swiper-container ref="containerRef" :init="false">
+          <swiper-slide v-for="(item, idx) in items" :key="idx" class="px-5">
+            <ServiceCard :item="item" />
+          </swiper-slide>
+        </swiper-container>
+      </ClientOnly>
+      <div class="flex items-center justify-center gap-5 mt-10">
+        <button
+          class="rounded-0 border-none bg-white py-2.5 px-6"
+          @click="swiper.prev()"
+        >
+          <img
+            src="~/assets/images/icons/arrowLeft.svg"
+            alt="Left arrow icon"
+          />
+        </button>
+
+        <button
+          class="rounded-0 border-none bg-white py-2.5 px-6"
+          @click="swiper.next()"
+        >
+          <img
+            src="~/assets/images/icons/arrowRight.svg"
+            alt="Right arrow icon"
+          />
+        </button>
+      </div>
+    </div>
+
+    <ServiceCard
+      v-for="item in items"
+      :key="item.title"
+      :item="item"
+      class="hidden lg:flex"
+    />
+  </SectionComponent>
+
+  <!-- Go back one slide -->
+  <button @click="swiper.prev()">Prev</button>
+  <!-- Go forward one slide -->
+  <button @click="swiper.next()">Next</button>
 </template>
 
-
 <script setup lang="ts">
-import ServiceCard from '@/components/pages/home/services/ServiceCardComponent.vue'
-import type { ServiceItem } from '~/types/services'
+  import ServiceCard from '@/components/pages/home/services/ServiceCardComponent.vue'
+  import type { ServiceItem } from '~/types/services'
 
-const items: ServiceItem[] = [
+  const items: ServiceItem[] = [
     {
-        icon: 'smile',
-        title: 'Smile Design',
-        description: 'A smile design is a cosmetic dental procedure that enhances the appearance of your smile.',
-        route: '/'
+      icon: 'smile',
+      title: 'Smile Design',
+      description:
+        'A smile design is a cosmetic dental procedure that enhances the appearance of your smile.',
+      route: '/',
     },
     {
-        icon: 'implant',
-        title: 'Dental Implants',
-        description: 'Dental implants are artificial tooth roots that provide a permanent base for fixed replacement teeth.',
-        route: '/'
+      icon: 'implant',
+      title: 'Dental Implants',
+      description:
+        'Dental implants are artificial tooth roots that provide a permanent base for fixed replacement teeth.',
+      route: '/',
     },
     {
-        icon: 'teeth',
-        title: 'Teeth Whitening',
-        description: 'Teeth whitening is a cosmetic procedure that lightens the color of your teeth.',
-        route: '/'
-    }
-]
+      icon: 'teeth',
+      title: 'Teeth Whitening',
+      description:
+        'Teeth whitening is a cosmetic procedure that lightens the color of your teeth.',
+      route: '/',
+    },
+  ]
 
+  const containerRef = ref(null)
+
+  const swiper = useSwiper(containerRef, {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 1,
+  })
+
+  onMounted(() => {
+    swiper.instance
+  })
 </script>
