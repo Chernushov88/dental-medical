06b10de5ee7ac9e0d1569cd22e47c907708afaa9
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-08-08 17:49:14 +0300

    fit: add z-index fix to the marked word component

--- src/components/pages/home/services/ServicesSliderSectionComponent.vue
+++ src/components/pages/home/services/ServicesSliderSectionComponent.vue
@@ -18,19 +18,19 @@ const items: ServiceItem[] = [
         icon: 'smile',
         title: 'Smile Design',
         description: 'A smile design is a cosmetic dental procedure that enhances the appearance of your smile.',
-        route: '/services/smile'
+        route: '/'
     },
     {
         icon: 'implant',
         title: 'Dental Implants',
         description: 'Dental implants are artificial tooth roots that provide a permanent base for fixed replacement teeth.',
-        route: '/services/dental-implants'
+        route: '/'
     },
     {
         icon: 'teeth',
         title: 'Teeth Whitening',
         description: 'Teeth whitening is a cosmetic procedure that lightens the color of your teeth.',
-        route: '/services/teeth-whitening'
+        route: '/'
     }
 ]
 
