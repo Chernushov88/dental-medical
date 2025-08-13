6ec685a6e17ec732e47784051f7cf09239d00654
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-08-13 08:59:20 +0300

    feat: create the section for send the user phone to connect

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,10 +3,13 @@
     <FirstScreen />
     <ServicesSliderSection />
     <PhoneFormSectionComponent />
+    <BookSection />
   </div>
 </template>
 
 <script setup lang="ts">
+import BookSection from '~/components/pages/home/book/BookSectionComponent.vue';
+
   import FirstScreen from '@/components/pages/home/firstScreen/FirstScreenComponent.vue'
   import ServicesSliderSection from '@/components/pages/home/services/ServicesSliderSectionComponent.vue'
 
