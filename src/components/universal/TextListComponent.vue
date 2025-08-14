bbdcaa97f99d0541884eca09761bfc27dbdbbb11
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-08-14 09:09:17 +0300

    feat: create the text list component

---
+++ src/components/universal/TextListComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<ul>
+    <li 
+        v-for="(item, key) in items"
+        :key="key"
+        class="flex items-center mb-4 last-of-type:mb-0"
+    >
+        <div class="mr-2.5 h-5 aspect-square flex items-center justify-center">
+            <img src="~/assets/images/icons/doneMark.svg" alt="List marker" />
+        </div>
+
+        <span class="text-b1 font-medium">
+            {{ item }}
+        </span>
+    </li>
+</ul>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    items: string[]
+}>();
+</script>
