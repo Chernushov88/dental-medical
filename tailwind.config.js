9d2cdd49d00f7cc354fcb98772d5809b743536f3
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-08-16 19:53:19 +0300

    style: add new aspect radio class to config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -124,7 +124,10 @@ export default {
           '0%': { transform: 'rotate(360deg)' },
           '100%': { transform: 'rotate(0deg)' },
         }
-      }
+      },
+      aspectRatio: {
+        '3/1.2': '3 / 1.2'
+      },
     },
   },
   plugins: [],
