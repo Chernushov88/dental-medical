5c56f55fc5c6f976c5b032f84beb6983499df4b6
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-08-16 21:53:37 +0300

    chore: show the Meet section on the home page

--- src/components/pages/home/meet/MeetSectionComponent.vue
+++ src/components/pages/home/meet/MeetSectionComponent.vue
@@ -2,7 +2,7 @@
     <SectionComponent
         fullWidth
         background
-        class="flex flex-col items-cente lg:py-14"
+        class="flex flex-col items-cente pt-16 pb-12 lg:py-14"
     >
         <div class="lg:text-center flex flex-col items-center">
             <SectionHeadingComponent class="capitalize">
@@ -13,26 +13,33 @@
                 We use only the best quality materials on the market in order to provide the best products to our patients.
             </DescriptionWrapperComponent>
         </div>
-
-        <client-only>
-            <div class="w-fit relative left-1/2 -translate-x-1/2">
-                <swiper-container ref="containerRef" class="w-screen">
+        <div class="hidden lg:block">
+            <client-only>
+                <swiper-container ref="containerRef" class="w-screen" :init="false">>
                     <swiper-slide 
                         v-for="(item, i) in items"
                         :key="i"
-                        class="!w-fit"
-                    >
-                        <SpecialistsCard v-bind="item" />
+                        class="!w-fit"> 
+                        <SpecialistsCard v-bind="item" /> 
                     </swiper-slide>
                 </swiper-container>
-            </div>
-        </client-only>
-
-        <div class="flex items-center justify-center gap-2.5 mt-10">
-            <PrevBtnComponent @click="swiper.prev()" />
-
-            <NextBtnComponent @click="swiper.next()" />
+            </client-only>
         </div>
+
+        <div class="flex flex-col items-center lg:hidden">
+            <div class="flex flex-col gap-8 w-full">
+                <template
+                    v-for="(item, i) in items"
+                    :key="i">
+                    <SpecialistsCard
+                        v-if="i < 2"
+                        v-bind="item"
+                    />
+                </template>
+            </div>
+            <ButtonComponent class="mt-10">View all Members</ButtonComponent>
+        </div>
+        
     </SectionComponent>
 </template>
 
@@ -40,13 +47,22 @@
 import type { TeamItem } from '~/types/team';
 import SpecialistsCard from './SpecialistsCardComponent.vue';
 
+
+
 const containerRef = ref(null)
 
 const swiper = useSwiper(containerRef, {
     loop: true,
     slidesPerView: 4,
     spaceBetween: 20,
+    autoplay: {
+      delay: 3000,
+    },
     centeredSlides: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
 })
 
 onMounted(() => swiper.instance)
@@ -76,5 +92,30 @@ const items: TeamItem[] = [
         photo: './pages/home/team/doctor4.png',
         profileLink: 'https://www.linkedin.com/'
     },
+    {
+        name: 'Jim Carry',
+        position: 'Orthodontist',
+        photo: './pages/home/team/doctor1.png',
+        profileLink: 'https://www.linkedin.com/'
+    },
+    {
+        name: 'Wade Warren',
+        position: 'Endodontist',
+        photo: './pages/home/team/doctor2.png',
+        profileLink: 'https://www.linkedin.com/'
+    },
+    {
+        name: 'Jenny Wilson',
+        position: 'Periodontist',
+        photo: './pages/home/team/doctor3.png',
+        profileLink: 'https://www.linkedin.com/'
+    },
+    {
+        name: 'Jacob Jones',
+        position: 'Pediatric Dentist',
+        photo: './pages/home/team/doctor4.png',
+        profileLink: 'https://www.linkedin.com/'
+    },
 ]
+
 </script>
