f09a202d929aadad2aa82faff362c06be0e720be
Serhii Chernyshov <sergei.chernyshov88@gmail.com>
2025-09-08 13:33:46 +0300

    chore: add SEO settings

--- src/components/universal/LogoComponent.vue
+++ src/components/universal/LogoComponent.vue
@@ -1,5 +1,5 @@
 <template>
- <nuxt-link to="/" class="flex items-center" >
-    <img src="~/assets/images/logo.svg" alt="">
+ <nuxt-link to="/" class="flex items-center" alt="WebStudio Chernyshov" title="Web Studio Chernyshov">
+    <h1 class="text-h3 text-blue-dark">Chernyshov</h1>
 </nuxt-link>
 </template>
