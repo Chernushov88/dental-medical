6660182735a487b7f717f2716894b1a900c6a13a
Serhii Chernyshov <chernushov88@gmail.com>
2025-12-16 18:00:41 +0200

    feat: add the Articles section to the blogs page

---
+++ src/components/pages/blogs/ArticlesSectionComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <DSection class="pb-6 lg:pb-25">
+        <div class="flex items-center justify-between mb-14">
+            <SectionHeadingComponent>
+                <MarkedWordComponent>
+                    Articles
+                </MarkedWordComponent>
+            </SectionHeadingComponent>
+
+            <span>Sort by: New</span>
+        </div>
+
+        <div class="grid lg:grid-cols-4 gap-5 mb-10">
+            <BlogItemCardComponent
+                v-for="(item, i) in items"
+                :key="i"
+                v-bind="item"
+                :class="i && 'hidden lg:block'"
+            />
+        </div>
+
+        <div class="flex justify-center">
+            <DButton>Check out more</DButton>
+        </div>
+    </DSection>
+</template>
+
+
+<script setup lang="ts">
+import { useBlogStore } from '~/store/useBlogStore';
+import BlogItemCardComponent from '~/components/pages/home/blog/BlogItemCardComponent.vue';
+
+const { items } = storeToRefs(useBlogStore())
+</script>
