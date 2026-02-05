7da2a1069654c1b4d2cee8c2c90227f44809ae32
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-05 10:56:11 +0200

    style: add  mobile  version to the main services section

--- src/components/pages/services/ServicesMainComponent.vue
+++ src/components/pages/services/ServicesMainComponent.vue
@@ -4,18 +4,20 @@
       :style="`height: calc(100% + ${scapeFromTop}px)`"
       class="bg-gradient-to-b from-blue-light to-white absolute w-screen bottom-0 left-1/2 -translate-x-1/2"
     />
-    <div ref="servicesSection" class="flex flex-col items-center">
+    <div ref="servicesSection" class="flex flex-col lg:items-center">
       <SectionHeadingComponent>
         <MarkedWordComponent>Services</MarkedWordComponent>
       </SectionHeadingComponent>
 
-      <DescriptionWrapperComponent class="max-w-[526px] text-center">
+      <DescriptionWrapperComponent
+        class="max-w-[526px] lg:text-center mb-12 lg:mb-20"
+      >
         We use only the best quality materials on the market in order to provide
         the best products to our patients.
       </DescriptionWrapperComponent>
     </div>
 
-    <div class="grid grid-cols-3 gap-x-14 gap-y-12 relative">
+    <div class="grid lg:grid-cols-3 gap-x-14 gap-y-8 lg:gap-y-12 relative">
       <ServiceCard v-for="(item, i) in items" :key="i" :item="item" />
     </div>
   </SectionComponent>
