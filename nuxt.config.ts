cc37d824640335def5b8333a22c68991a3c41ba1
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-05 11:20:23 +0200

    chore: add vueuse to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
   ],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-swiper'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-swiper', '@vueuse/nuxt',],
   srcDir: 'src/.',
   components: {
     dirs: [
