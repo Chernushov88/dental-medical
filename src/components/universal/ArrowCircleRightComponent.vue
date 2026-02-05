435c3b89ee8703b5d01b4e408cd5ad03e5b43f8e
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-05 11:30:36 +0200

    chore:  add ArrowCircleRightComponent

---
+++ src/components/universal/ArrowCircleRightComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <img
+    src="~/assets/images/icons/arrorRightCircle.svg"
+    alt="Arror right"
+    :class="white && 'invert'"
+  />
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    white?: boolean;
+  }>(),
+  {
+    white: false,
+  },
+);
+</script>
