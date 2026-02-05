a1185d5f0819c5a2872245eb619440abbc52bbde
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-05 12:07:21 +0200

    feat: add the new "Contact form" section to the Services page

---
+++ src/components/pages/services/ContactFormComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <SectionComponent
+    full-width
+    background
+    bg-color="bg-blue-dark"
+    class="flex items-center justify-center lg:gap-20 lg:py-12"
+  >
+    <div class="max-w-[570px] text-white shrink-0">
+      <SectionHeadingComponent class="lg:!mb-3 capitalize">
+        Leave your worries at the door and enjoy a healthier, more
+        <MarkedWordComponent>precise smile</MarkedWordComponent>
+      </SectionHeadingComponent>
+
+      <DescriptionWrapperComponent class="text-white lg:!mb-8 max-w-[500px]">
+        We use only the best quality materials on the market in order to provide
+        the best products to our patients, So don’t worry about anything and
+        book yourself.
+      </DescriptionWrapperComponent>
+
+      <Button>
+        <span class="flex items-center">
+          <span class="mr-2">Learn More</span>
+
+          <ArrowCircleRightComponent white />
+        </span>
+      </Button>
+    </div>
+
+    <div class="max-w-[400px] shrink-0 w-full">
+      <FastConnectFormComponent />
+    </div>
+  </SectionComponent>
+</template>
--- src/components/universal/SectionComponent.vue
+++ src/components/universal/SectionComponent.vue
@@ -1,24 +1,26 @@
 <template>
-    <section
-        class="px-5 max-w-content mx-auto"
-        :class="fullWidth && 'relative'"
-    >
-        <div
-            v-if="fullWidth"
-            class="-z-10 pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full w-screen"
-            :class="background && 'bg-blue-light'"    
-        />
+  <section class="px-5 max-w-content mx-auto" :class="fullWidth && 'relative'">
+    <div
+      v-if="fullWidth"
+      class="-z-10 pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full w-screen"
+      :class="background && bgColor"
+    />
 
-        <slot />
-    </section>
+    <slot />
+  </section>
 </template>
 
 <script setup lang="ts">
-withDefaults(defineProps<{
-    fullWidth?: boolean
-    background?: boolean
-}>(), {
+withDefaults(
+  defineProps<{
+    fullWidth?: boolean;
+    background?: boolean;
+    bgColor?: string;
+  }>(),
+  {
     fullWidth: false,
-    background: false
-})
+    background: false,
+    bgColor: 'bd-blue-light',
+  },
+);
 </script>
