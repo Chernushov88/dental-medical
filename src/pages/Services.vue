f89b5b5886ba349843d56154921d7c86c05be493
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-06 10:18:18 +0200

    chore: add other already implemented sections to the service page

--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -2,12 +2,20 @@
   <div>
     <ServicesMain />
     <ContactFormComponent />
+    <PlayListComponent class="!py-25" />
+    <MeetSectionComponent />
+    <AsksSectionComponent />
+    <AskToActionSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import ContactFormComponent from '~/components/pages/services/ContactFormComponent.vue';
 import ServicesMain from '~/components/pages/services/ServicesMainComponent.vue';
+import PlayListComponent from '~/components/pages/home/playListPreview/PlaylistPreviewComponent.vue';
+import MeetSectionComponent from '~/components/pages/home/meet/MeetSectionComponent.vue';
+import AsksSectionComponent from '~/components/pages/home/asks/AsksSectionComponent.vue';
+import AskToActionSection from '~/components/pages/home/askToAction/AskToActionSection.vue';
 </script>
 
 <style lang="scss" scoped></style>
