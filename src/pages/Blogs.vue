a7516e90f2afc9f1e5a953348332403222abd3cf
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-06 15:53:30 +0200

    chore: add other already created section from design to the blogs page

--- src/pages/Blogs.vue
+++ src/pages/Blogs.vue
@@ -2,12 +2,18 @@
   <div>
     <BlogsMainComponent />
     <ArticlesSectionComponent />
+    <PlaylistPreviewComponent />
+    <AsksSectionComponent />
+    <AskToActionSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import ArticlesSectionComponent from '~/components/pages/blogs/ArticlesSectionComponent.vue';
 import BlogsMainComponent from '~/components/pages/blogs/BlogsMainComponent.vue';
+import AsksSectionComponent from '~/components/pages/home/asks/AsksSectionComponent.vue';
+import PlaylistPreviewComponent from '~/components/pages/home/playListPreview/PlaylistPreviewComponent.vue';
+import AskToActionSection from '~/components/pages/home/askToAction/AskToActionSection.vue';
 </script>
 
 <style scoped></style>
