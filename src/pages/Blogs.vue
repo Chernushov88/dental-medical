352b628c11c98b48d33922241f3596180c2f497f
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-06 15:43:29 +0200

    chore: show new section on the blogs page

--- src/pages/Blogs.vue
+++ src/pages/Blogs.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        Blogs
-    </div>
+  <div>
+    <BlogsMainComponent />
+    <ArticlesSectionComponent />
+  </div>
 </template>
 
 <script setup lang="ts">
-
+import ArticlesSectionComponent from '~/components/pages/blogs/ArticlesSectionComponent.vue';
+import BlogsMainComponent from '~/components/pages/blogs/BlogsMainComponent.vue';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
