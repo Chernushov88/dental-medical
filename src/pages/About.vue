802e30b59e6e14b640766bbb9d86dbc99d6a3424
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-14 22:46:58 +0200

    feat: implement new  RandomInfo Component

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,10 +2,12 @@
   <div>
     <AboutUsComponent />
     <OurDoctorsComponent />
+    <RandomInfoComponent />
   </div>
 </template>
 
 <script setup lang="ts">
 import AboutUsComponent from '~/components/pages/about-as/AboutUsComponent.vue';
 import OurDoctorsComponent from '~/components/pages/about-as/OurDoctorsComponent.vue';
+import RandomInfoComponent from '~/components/pages/about-as/RandomInfoComponent.vue';
 </script>
