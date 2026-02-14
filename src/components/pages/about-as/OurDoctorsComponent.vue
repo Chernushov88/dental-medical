4f48ddc91e8c82576e8425d5ea5b6c05fddf60cb
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-14 22:21:58 +0200

    feat: implement the component to show our doctors on the "about us" page

---
+++ src/components/pages/about-as/OurDoctorsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <SectionComponent>
+    <div class="flex flex-col items-center">
+      <SectionHeadingComponent class="capitalize">
+        Meet our
+        <MarkedWordComponent>specialists</MarkedWordComponent>
+      </SectionHeadingComponent>
+
+      <DescriptionWrapperComponent class="lg:text-center max-w-[480px] mb-0">
+        We use only the best quality materials on the market in order to provide
+        the best products to our patients.
+      </DescriptionWrapperComponent>
+    </div>
+
+    <DoctorCardComponent v-for="(item, i) in items" :key="i" v-bind="item" />
+  </SectionComponent>
+</template>
+
+<script setup>
+import { useDoctorsStore } from '~/store/useDoctorsStore';
+import DoctorCardComponent from './DoctorCardComponent.vue';
+
+const { items } = useDoctorsStore();
+</script>
+
+<style lang="scss" scoped></style>
