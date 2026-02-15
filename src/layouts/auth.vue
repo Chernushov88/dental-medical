ede5215f7fe5b31384930dddeddcbca43fc8bcfa
Serhii Chernyshov <chernushov88@gmail.com>
2026-02-15 18:05:14 +0200

    feat: implement authorization layout

---
+++ src/layouts/auth.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    :class="isReady ? 'opacity-100' : 'opacity-0'"
+    class="default-layout flex flex-col min-h-screen max-w-max mx-auto lg:px-4 transition-all duration-500"
+  >
+    <HeaderComponent class="fixed left-1/2 -translate-x-1/2" />
+
+    <main class="flex-auto">
+      <slot />
+    </main>
+
+    <FooterComponent />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTransitionSections } from '#build/imports';
+
+const { isReady } = useTransitionSections();
+</script>
+
+<style lang="scss" scoped></style>
